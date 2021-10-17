6319bf26a455856fbaedfa885b4fda28f434ea6b
Odeh <abuzaid163@hotmail.com>
2021-10-18 00:26:02 +0300

    Solution

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -17,31 +17,32 @@ class GameLogic:
   @staticmethod
   def calculate_score(rolled):
     rolled = Counter(rolled)
+    score = check_straight_or_pairs(rolled)
+    
+    if not score:
+      for number in rolled:
+        appears = rolled[number]
+        if appears >= 3 and number == 1:score+= (number *1000) * (appears-2)
+        if appears >= 3 and number != 1:score+= (number *100) * (appears-2)
+        if appears < 3 and number == 1: score+= 100*appears
+        if appears < 3 and number == 5: score+= 50*appears
+    return score
+  
+
+  @staticmethod
+  def roll_dice(roll=6):
+    rolls = tuple(randint(1,6) for _ in range(roll))
+    return rolls
+
+def check_straight_or_pairs(rolled):
     score = 0
     if len(rolled) == 6:
       straight =  all(value == 1 for value in rolled.values())
       if straight:
         score = 1500
+
     if len(rolled) == 3:
       three_pairs = all(value == 2 for value in rolled.values())
       if three_pairs:
         score = 750 * 2
-    if score == 0:
-      for number in rolled:
-        appears = rolled[number]
-        if appears >= 3:
-          if number == 1:
-            score+= (number *1000) * (appears-2)
-          else:   
-            score+= (number *100) * (appears-2)
-        else:
-          if number == 1: 
-            score+= 100*appears
-          if number == 5: 
-            score+= 50*appears
     return score
-  
-  @staticmethod
-  def roll_dice(roll=6):
-    rolls = tuple(randint(1,6) for _ in range(roll))
-    return rolls
