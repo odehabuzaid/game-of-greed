9e9c6635b3b5eca5afed593b56817be3f0c860bc
Odeh <abuzaid163@hotmail.com>
2021-10-18 20:06:29 +0300

    Solution

--- game_of_greed/__main__.py
+++ game_of_greed/__main__.py
@@ -1,8 +1,4 @@
-from pyfiglet import Figlet
-from termcolor import colored
+from game import Game
 
 if __name__ == "__main__":
-    logo = Figlet(font="standard")
-    print(colored(logo.renderText("Game of Greed"), "red"))
-    print(colored(("=" * 70), "red"))
-    print(colored(("Welcome to Game of Greed!"), "white"))
+    Game.play()
---
+++ game_of_greed/game.py
@@ -0,0 +1,60 @@
+from game_of_greed.banker import Banker
+from game_of_greed.game_logic import GameLogic
+
+#
+
+
+class Game:
+    def __init__(self):
+        self.rounds = 1
+        self.dice = 6
+        self.score = 0
+        self.banker = Banker()
+
+    def play(self, roller=6):
+        user_input = ""
+        if self.rounds == 1:
+            print("Welcome to Game of Greed")
+            print("(y)es to play or (n)o to decline")
+            user_input = input("> ")
+        if user_input == "n":
+            print("OK. Maybe another time")
+            exit()
+        else:
+            print("Starting round {}".format(self.rounds))
+            print("Rolling 6 dice...")
+            #
+            rolls = roller(self.dice)
+            rolls = " ".join(map(str, rolls))
+            print("*** {} ***".format(rolls))
+            print("Enter dice to keep, or (q)uit:")
+            user_input = input("> ")
+            if user_input == "q":
+                print(
+                    "Thanks for playing. You earned {} points".format(
+                        self.banker.balance
+                    )
+                )
+            else:
+                calculated = GameLogic.calculate_score(tuple([4, 2, 6, 4, 6, 5]))
+                self.banker.shelf(calculated)
+                self.calculate_dice(user_input)
+                print(
+                    "You have {} unbanked points and {} dice remaining".format(
+                        calculated, self.dice
+                    )
+                )
+                print("(r)oll again, (b)ank your points or (q)uit:")
+                user_input = input("> ")
+                if user_input == "b":
+                    print(
+                        "You banked {} points in round {}".format(
+                            calculated, self.rounds
+                        )
+                    )
+                    print("Total score is {} points".format(self.banker.bank()))
+                    self.rounds += 1
+                    self.play()
+
+    def calculate_dice(self, input):
+        self.dice -= len(input)
